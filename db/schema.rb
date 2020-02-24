9f3b4cd2fb58e5c3e328ca31604fd23506e13af7
Pedro Batista <pedrobatista.420@gmail.com>
2020-02-24 17:05:37 +0000

    Pushing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_24_163856) do
 
   # These are extensions that must be enabled in order to support this database
@@ -42,5 +41,4 @@ ActiveRecord::Schema.define(version: 2020_02_24_163856) do
   end
 
   add_foreign_key "dragons", "users"
-
 end
