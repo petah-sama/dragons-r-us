72892716d6de6827e98f631abf7f1d4e297ffa72
Hugo Veloso <hugopcveloso@gmail.com>
2020-02-27 15:36:00 +0000

    map style-light

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,8 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/light-v10'
+
   });
 };
 
